19f1e683bd25e58112aa83692c34e1e38ac3864b
Janna Denz Estrada <janna.estrada@jmc.edu.ph>
2025-02-03 11:16:53 +0800

    Add files via upload

---
+++ myfirstC#program.cs
@@ -0,0 +1,23 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+class Calculator
+{
+    public static void Main(string[] args)
+    {
+         //Data Types
+         string message = "Tulgon pako Sir";
+         int num = 12312;
+         float pi = 3.14f;
+         char letter = 'j';
+         bool flag = true; //false or true
+         
+        Console.WriteLine("Dae ay kabalaka diyan bi"); // output
+        Console.WriteLine("Dili ko mu ulaw ko naa gani si crush "); //output
+        string myinput = Console.ReadLine(); // input
+        Console.WriteLine(myinput);
+    }
+    
+}
